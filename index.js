fbd8832e3f0d6ca5d46e39c03d94f1372f420c1c
Glen Pegado <glenpegado@gmail.com>
2019-05-28 15:07:28 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//variable to sort in asceding and descending order
+let sortDirection = false;
+
+//Dummy data, could be an API, sql data etc...
+let personData = [
+    {name: 'Glen', age: 25},
+    {
+        name: 'Michael',
+        age: 32
+    },
+    {
+        name: 'Jennifer',
+        age: 42
+    },
+    {
+        name: 'Dylan',
+        age: 34
+    },
+]
+
+loadTableData(personData)
+
+function loadTableData(personData){
+    const tableBody = document.getElementById('tableData')
+    let dataHtml = '';
+
+    for(let person of personData){
+        //this could be replicated using jquery
+        dataHtml += `<tr><td>${person.name}</td><td>${person.age}</td></tr>`
+    }
+    console.log(dataHtml)
+
+    tableBody.innerHtml = dataHtml
+}
